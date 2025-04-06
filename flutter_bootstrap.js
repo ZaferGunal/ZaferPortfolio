d225203342df907e6600a02ff1fb4d76739598ec
ZaferGunal <122797651+ZaferGunal@users.noreply.github.com>
2025-04-06 18:55:23 +0300

    second

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3902622512"
+    serviceWorkerVersion: "2209897569"
   }
 });
